8f00d56b1008d2ae96ca36402fb92be73c7916cf
Samuel Valentine <valentinesamuel2580@gmail.com>
2021-05-09 11:58:54 +0100

    (FEAT): Created the reducer, store and actions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterButtonsComponent } from './counter/counter-buttons/counter-buttons.component';
 import { CounterOutputComponent } from './counter/counter-output/counter-output.component';
 import { CounterComponent } from './counter/counter/counter.component';
+import { CounterReducer } from './counter/state/counter.reducer';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { CounterComponent } from './counter/counter/counter.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ counter: CounterReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
