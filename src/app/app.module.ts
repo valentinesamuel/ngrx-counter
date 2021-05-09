c95e2527edf244659aedee8facb22b7b4a526888
Samuel Valentine <valentinesamuel2580@gmail.com>
2021-05-09 11:09:35 +0100

    (FEAT): Created the counter buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterButtonsComponent } from './counter/counter-buttons/counter-buttons.component';
+import { CounterOutputComponent } from './counter/counter-output/counter-output.component';
+import { CounterComponent } from './counter/counter/counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CounterButtonsComponent,
+    CounterOutputComponent,
+    CounterComponent,
   ],
   imports: [
     BrowserModule,
